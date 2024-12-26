10ce0319d1c77ed5a15ce23a96bdd2ceb11d5197
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2024-12-26 17:37:47 +0700

    admin dashboard page

--- src/components/admin-dashboard-card/index.tsx
+++ src/components/admin-dashboard-card/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Card, Col, Row } from 'antd';
+import Title from 'antd/es/typography/Title';
 
-const AdminDashboardCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface iAdminDashboardCard {
+    title: string;
+    valueNumber: number;
+    percent: number;
+    description: string
+    icon: React.ReactNode
+    arrowIcon: React.ReactNode
+}
+
+const AdminDashboardCard = ({ title, valueNumber, percent, description, icon, arrowIcon }: iAdminDashboardCard) => {
+    return (
+        <Card style={{ width: 300 }}>
+            <Row>
+                <Col span={20}>
+                    <Title level={5}>{title}</Title>
+                </Col>
+                <Col span={4}>
+                    <div>
+                        {icon}
+                    </div>
+                </Col>
+            </Row>
+            <Title level={2}>{valueNumber}</Title>
+            <Row gutter={5} className='flex items-center text-center'>
+                <Col  span={4}>{arrowIcon}</Col>
+                <Col className='text-green-500' span={4}>{percent}%</Col>
+                <Col span={16}>{description}</Col>
+            </Row>
+        </Card>
+    )
 }
 
 export default AdminDashboardCard
--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,12 +1,122 @@
+import { Col, Row } from 'antd'
 import Title from 'antd/es/typography/Title'
-import React from 'react'
+import { AdminDashboardCard } from '../../../components'
+import NorthEastOutlinedIcon from '@mui/icons-material/NorthEastOutlined';
+import GroupIcon from '@mui/icons-material/Group';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { Select } from 'antd';
 
+const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+};
 const AdminDashboard = () => {
-  return (
-    <div className='container mx-auto mt-10'>
-      <Title level={1}>Dashboard</Title>
-    </div>
-  )
+    return (
+        <div className='container mx-auto mt-10'>
+            <Title level={1}>Dashboard</Title>
+            <Row>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+            </Row>
+
+            <Row className='mt-10'>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+            </Row>
+
+            <Row className='mt-10'>
+                <Col span={16}>
+                    <div className='grid grid-cols-2'>
+                    <Title level={4}>Total Downloads</Title>
+                    <Select
+                        defaultValue="2021"
+                        style={{ width: 100 }}
+                        onChange={handleChange}
+                        options={[
+                            {
+                                options: [
+                                    { label: <span>2022</span>, value: '2022' },
+                                    { label: <span>2021</span>, value: '2021' },
+                                ],
+                            },
+                        ]}
+                    />
+                    </div>
+                    <BarChart
+                        xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                        width={800}
+                        height={480}
+                    />
+                </Col>
+                <Col span={8}>
+                    <AdminDashboardCard
+                        arrowIcon={<NorthEastOutlinedIcon style={{ fontSize: '50px' }} className='text-green-500' />}
+                        title='Total User'
+                        icon={<GroupIcon style={{ fontSize: '50px' }} className=' text-purple-500' />}
+                        valueNumber={10000}
+                        percent={5}
+                        description='Up from Yesterday'
+                    />
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default AdminDashboard
